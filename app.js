04c3b3170f5d71522218f787ed4b95ed565c92d9
Kody <kydokody@gmail.com>
2025-02-02 02:10:57 +0100

    Update App.js to original state

--- app.js
+++ app.js
@@ -9,26 +9,27 @@ const userStoriesRoutes = require('./routes/userStoriesRoutes')
 
 const app = express();
 
-const corsOptions = {
-   origin: [
-     'https://www.agilflow.app',  // URL de production Vercel
-   //   'http://127.0.0.1:5173',                  // URL de développement local pour Vite
-   //   'http://127.0.0.1:3000'                   // Alternative pour le port 3000
-   ],
-   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
-   allowedHeaders: ['Content-Type', 'Authorization'],
-   credentials: true,                          // Permet l'envoi de cookies
-   optionsSuccessStatus: 204                   // Pour la compatibilité avec certains navigateurs
- };
+// const corsOptions = {
+//    origin: [
+//      'https://www.agilflow.app',  // URL de production Vercel
+//    //   'http://127.0.0.1:5173',                  // URL de développement local pour Vite
+//    //   'http://127.0.0.1:3000'                   // Alternative pour le port 3000
+//    ],
+//    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+//    allowedHeaders: ['Content-Type', 'Authorization'],
+//    credentials: true,                          // Permet l'envoi de cookies
+//    optionsSuccessStatus: 204                   // Pour la compatibilité avec certains navigateurs
+//  };
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
+app.use(cors);
 
-// app.use((req, res, next) => {
-//    res.header("Access-Control-Allow-Origin", "*");
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-//     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH");
-//     next();
-// })
+app.use((req, res, next) => {
+   res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH");
+    next();
+})
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
