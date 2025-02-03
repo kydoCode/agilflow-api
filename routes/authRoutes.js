fb448178e69240292c588e121dac599654414340
Kody <157696416+kydoCode@users.noreply.github.com>
2025-02-03 10:49:51 +0100

    Fix configuration issues

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
+const cors = require('cors');
 
-
+router.options('*', cors()); // Ajoutez cette ligne
 router.post('/login', authController.login);
 router.post('/register', authController.register);
 
--- routes/userStoriesRoutes.js
+++ routes/userStoriesRoutes.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 const userStoriesController = require('../controllers/userStoriesController');
 const authMiddleware = require('../middleware/auth');
+const cors = require('cors');
 
+router.options('*', cors()); // Ajoutez cette ligne
 router.get('/', authMiddleware, userStoriesController.getUserStories);
 router.get('/:id', authMiddleware, userStoriesController.getUserStoryById);
 router.post('/', authMiddleware, userStoriesController.createUserStory);
