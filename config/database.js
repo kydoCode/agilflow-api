02ab66f4b8c19309f809cdf7414eec87ac24de37
Kody <kydokody@gmail.com>
2025-02-03 10:49:51 +0100

    Fix configuration issues

--- config/database.js
+++ config/database.js
@@ -1,5 +1,6 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
+const pg = require('pg');
 
 // const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
 //    host: process.env.DB_HOST,
