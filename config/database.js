d1937d135afa348aa68eadd02f31a008a5d5cd33
kydoCode <157696416+kydoCode@users.noreply.github.com>
2025-02-03 22:05:02 +0100

    Chore: Configure PG module

--- config/database.js
+++ config/database.js
@@ -1,13 +1,6 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
-// const pg = require('pg');
-
-// const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-//    host: process.env.DB_HOST,
-//    dialect: 'mysql',
-//    logging: false,
-// });
-// console.log("DATABASE_URL:", process.env.DATABASE_URL);
+import pg from 'pg'
 
 if (!process.env.DATABASE_URL) {
   console.error('DATABASE_URL is not defined in environment variables');
@@ -16,6 +9,7 @@ if (!process.env.DATABASE_URL) {
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
+    dialectModule: pg,
     dialectOptions: {
       ssl: {
         require: true,
